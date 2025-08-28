be4aaa5a11d1d88ad1be3a89605e185711accf6a
vercah <vercaposta@seznam.cz>
2025-08-28 12:55:35 +0200

    #1 construct welsh-powell greedy algorithm

--- src/greedy.cpp
+++ src/greedy.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// ---------- DIMACS reader (stdin) ----------
+// ---- DIMACS reader ----
 bool read_dimacs(int& n, int& m, vector<vector<int>>& adj) {
     ios::sync_with_stdio(false); //disconnects c++ from c (scanf, printf), to make it faster
     cin.tie(nullptr); // stops cin from flusing cout before reading
@@ -45,4 +45,60 @@ bool read_dimacs(int& n, int& m, vector<vector<int>>& adj) {
         adj[i] = vector<int>(tmp[i].begin(), tmp[i].end());
     } 
     return true;
+}
+
+// ---- Greedy coloring ------
+vector<int> greedy_color(const vector<vector<int>>& adj, const vector<int>& order) {
+    int n = (int)adj.size();
+    vector<int> color(n, -1), mark(n, -1); // n colors, initially -1; mark tracks forbidden colors
+    int stamp = 0; //timestamp to avoid clearing mark each time
+    for (int v : order) {
+        stamp++;
+        for (int u : adj[v]) { if (color[u] != -1) mark[color[u]] = stamp;}
+        int c = 0;
+        while (c < n && mark[c] == stamp) c++;
+        color[v] = c;
+    }
+    return color;
+}
+
+// sort vertices by descending degree and vertex id
+vector<int> desc_degree_order(const vector<vector<int>>& adj) {
+    int n = (int)adj.size();
+    vector<int> order(n);
+    iota(order.begin(), order.end(), 0);  // fill with 0..n-1
+    sort(order.begin(), order.end(), [&](int a, int b) { //capture by reference from adj
+        if (adj[a].size() != adj[b].size())
+            return adj[a].size() > adj[b].size(); // higher degree first
+        return a < b; // tie: smaller index first
+    });
+    return order;
+}
+
+// ---- Main ------
+// uses the welsh-powell algo
+int main(int argc, char** argv) {
+    string dot_path = "graph_colored.dot"; //default
+    for (int i=1; i<argc; ++i) { //arg[0] is program name
+        string a = argv[i];
+        if (a == "--dot" && i+1 < argc) { dot_path = argv[++i]; } //user dot filename
+        else if (a == "--help" || a == "-h") {
+            cerr << "Usage: color [--dot out.dot] < input.col\n";
+            return 0;
+        }
+    }
+
+    int n, m;
+    vector<vector<int>> adj;
+    if (!read_dimacs(n, m, adj)) { cerr << "Failed to read DIMACS from input file.\n"; return 1; }
+
+    vector<int> order = desc_degree_order(adj);
+
+    vector<int> color = greedy_color(adj, order);
+
+    int k = 0;
+    for (int c: color) {k = max(k, c+1);}
+    cout << k << "\n"; // number of colors used
+    for (int i=0; i<n; ++i) { cout << color[i] << (i+1==n ? '\n':' ');}
+
 }
